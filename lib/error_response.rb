993e3638c7f987dbe71342525dbaa7618cdfc1e5
jameslee <jameslee@kdanmobile.com>
2019-08-12 12:06:37 +0800

    show message if key not found

--- lib/error_response.rb
+++ lib/error_response.rb
@@ -3,21 +3,21 @@ class ErrorResponse < Settingslogic
   source "lib/error_response.yml"
 
   def call(key, message=nil)
-    error_data = send(key)
+    error_data = self[key]
 
-    if error_data.present?
-      status = error_data['error_code'][0..2]
-      {
-        status: status,
-        json: error_data
-      }
-    else
+    if error_data.nil?
       {
         status: 500,
         json: {
           error_code: 50000,
           error_message: message
         }
+      }
+    else
+      status = error_data['error_code'][0..2]
+      {
+        status: status,
+        json: error_data
       }
     end
   end
