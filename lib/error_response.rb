91024cdf651ea6c50ed43a2bf1c9d706dbe68c44
jameslee <jameslee@kdanmobile.com>
2019-08-12 16:15:18 +0800

    add to_hash method

--- lib/error_response.rb
+++ lib/error_response.rb
@@ -3,7 +3,7 @@ class ErrorResponse
   GEM_ROOT = File.expand_path("../..", __FILE__)
   YAML_PATH = 'lib/error_response.yml'
 
-  def self.call(key, message=nil)
+  def self.to_api(key, message=nil)
     string_hash = YAML.load_file(File.join(GEM_ROOT,YAML_PATH))
     err_json = string_hash[key.to_s] || {'error_code' => 500000, 'error_message' => message}
 
@@ -12,5 +12,10 @@ class ErrorResponse
       'status' => status,
       'json' => err_json
     }
+  end
+
+  def self.to_hash(key)
+    string_hash = YAML.load_file(File.join(GEM_ROOT,YAML_PATH))
+    string_hash[key.to_s]
   end
 end
