d91e9663d4e81154217dd6f4bf0673315518c6a9
jameslee <jameslee@kdanmobile.com>
2019-08-12 10:56:14 +0800

    read message when 500 error

--- error_response.gemspec
+++ error_response.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'error_response'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.date        = '2019-08-12'
   s.summary     = "A tool for API error response"
   s.description = "use for error_response"
--- lib/error_response.rb
+++ lib/error_response.rb
@@ -2,18 +2,23 @@ require 'settingslogic'
 class ErrorResponse < Settingslogic
   source "lib/error_code.yml"
 
-  def api_format(key)
+  def call(key, message=nil)
     error_data = send(key)
-    status = fetch_status(error_data['error_code'])
-    {
-      status: status,
-      json: error_data
-    }
-  end
-  
-  private
-  def fetch_status(error_code)
-    return error_code if error_code == 500
-    error_code[0..2]
+
+    if error_data.present?
+      status = error_data['error_code'][0..2]
+      {
+        status: status,
+        json: error_data
+      }
+    else
+      {
+        status: 500,
+        json: {
+          error_code: 50000,
+          error_message: message
+        }
+      }
+    end
   end
 end
