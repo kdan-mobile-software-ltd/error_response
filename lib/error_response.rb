a6b5a8bd0316faae49314bf243fab89c93ae7c4e
jameslee <jameslee@kdanmobile.com>
2019-08-12 09:19:26 +0800

    add api_format

--- lib/error_response.rb
+++ lib/error_response.rb
@@ -1,4 +1,19 @@
 require 'settingslogic'
 class ErrorResponse < Settingslogic
   source "lib/error_code.yml"
+
+  def api_format(key)
+    error_data = send(key)
+    status = fetch_status(error_data['error_code'])
+    {
+      status: status,
+      json: error_data
+    }
+  end
+  
+  private
+  def fetch_status(error_code)
+    return error_code if error_code == 500
+    error_code[0..2]
+  end
 end
