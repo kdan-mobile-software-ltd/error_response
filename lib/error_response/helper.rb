24ee83221a733e0c9f28af82c2d8a2d793a936ef
JiaRou Lee <laura34963@kdanmobile.com>
2026-06-03 14:22:45 +0800

    Fix by rubocop

--- lib/error_response/helper.rb
+++ lib/error_response/helper.rb
@@ -8,7 +8,7 @@ module ErrorResponse
 
     # Ensure RequestError is always handled by this helper, even when
     # controllers define broad handlers like rescue_from Exception later.
-    def rescue_with_handler(exception, *args, **kwargs, &block)
+    def rescue_with_handler(exception, *args, **kwargs, &)
       if exception.is_a?(RequestError)
         error_response(exception.key, exception.error_message, exception.error_data)
         return exception
